3f35b27e4783143914e071d76857197cfec86145
Gio Lodi <giovanni.lodi42@gmail.com>
2020-09-17 15:37:17 +1000

    Add SwiftDate as a dependency

--- xchammer-pods-issue/App/Sources/AppDelegate.swift
+++ xchammer-pods-issue/App/Sources/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwiftDate
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // This is just a test to verify that the SwiftDate framework has been
+        // built and linked properly.
+        //
+        // This should print: "SwiftDate test: true" in the STDOUT logs of the
+        // `bazel run` command
+        print("SwiftDate test: \(Date().compare(.isToday))")
         return true
     }
 }
